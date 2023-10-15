e5d58c120119467e65ccfbb6cdfb702ba684c439
Ha Thi Lanh <lanhhthe163349@fpt.edu.vn>
2023-10-15 23:55:57 +0700

    create new project

---
+++ app/src/main/java/vn/fpt/edu/vn/matranoso/custom/TXVOVuong.java
@@ -0,0 +1,24 @@
+package vn.fpt.edu.vn.matranoso.custom;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class TXVOVuong extends TextView {
+    public TXVOVuong(Context context){
+        super(context);
+    }
+    public TXVOVuong(Context context, AttributeSet attrs){
+        super(context,attrs);
+    }
+    public  TXVOVuong(Context context, AttributeSet attrs, int defStyleAttr){
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int a = this.getMeasuredWidth();
+        setMeasuredDimension(a,a);
+    }
+}
